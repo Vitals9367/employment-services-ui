90cf28065a145faaff1f702e4f16ae5f3bfb6e26
Jussiles <juss.leskinen@gmail.com>
2022-05-31 09:09:14 +0300

    Add scss breakpoint variables.

--- src/components/liftupWithImage/LiftupWithImage.tsx
+++ src/components/liftupWithImage/LiftupWithImage.tsx
@@ -15,14 +15,30 @@ interface LiftupWithImageProps {
   field_liftup_with_image_desc: DrupalFormattedText
 }
 
+// TODO: Get Drupal Image styles dimensions programmatically.
+const imageStyleDimensions = {
+  '23_10_l_width': 1440,
+  '23_10_l_height': 626,
+  '3_2_m_width': 1024,
+  '3_2_m_height': 683
+}
+
 function LiftupWithImage(props: LiftupWithImageProps): JSX.Element {
   const { field_liftup_with_image_title, field_liftup_with_image_image, field_liftup_with_image_design: design, field_liftup_with_image_desc } = props
-  const textAlign = design === 'image-on-right' || design == 'image-on-right-secondary' || design === 'background-text-on-left' ? styles.textLeft : styles.textRight
-  const contentBgSecondary = design ===  'image-on-right-secondary' || design == 'image-on-left-secondary' ? styles.contentBgSecondary : ''
+  const textAlign = design === 'image-on-right' || design == 'image-on-right-secondary' || design === 'background-text-on-left'
+    ? styles.textLeft
+    : styles.textRight
+  const contentBgSecondary = design ===  'image-on-right-secondary' || design == 'image-on-left-secondary'? styles.contentBgSecondary : ''
   const imageFullWidth = design ===  'background-text-on-right' || design == 'background-text-on-left' ? styles.imageFullWidth : ''
-  const imageStyle = design ===  'background-text-on-right' || design == 'background-text-on-left' ? getImageUrl(field_liftup_with_image_image?.field_media_image?.image_style_uri?.['23_10_l']) : getImageUrl(field_liftup_with_image_image?.field_media_image?.image_style_uri?.['3_2_m'])
-  const imageWidth = design ===  'background-text-on-right' || design == 'background-text-on-left' ? 1440 : 1024
-  const imageHeight = design ===  'background-text-on-right' || design == 'background-text-on-left' ? 626 : 683
+  const imageStyle = design ===  'background-text-on-right' || design == 'background-text-on-left'
+    ? getImageUrl(field_liftup_with_image_image?.field_media_image?.image_style_uri?.['23_10_l'])
+    : getImageUrl(field_liftup_with_image_image?.field_media_image?.image_style_uri?.['3_2_m'])
+  const imageWidth = design ===  'background-text-on-right' || design == 'background-text-on-left'
+    ? imageStyleDimensions['23_10_l_width']
+    : imageStyleDimensions['3_2_m_width']
+  const imageHeight = design ===  'background-text-on-right' || design == 'background-text-on-left'
+    ? imageStyleDimensions['23_10_l_height']
+    : imageStyleDimensions['3_2_m_height']
 
   return (
     <div className='component'>
@@ -32,8 +48,8 @@ function LiftupWithImage(props: LiftupWithImageProps): JSX.Element {
             src={imageStyle}
             alt={field_liftup_with_image_image?.field_media_image?.resourceIdObjMeta?.alt}
             layout='responsive'
-            width={imageWidth}
-            height={imageHeight}
+            width={`${imageWidth}px`}
+            height={`${imageHeight}px`}
           />
         </div>
         <div className={styles.liftupWithImageContentWrapper}>
