bb9a53317a8e4a493d80ccfb8299d1f99b4b952f
Krister Haverinen <krister.haverinen@siili.com>
2022-10-20 14:05:00 +0300

    THF-231: Adding HDS Cookie Consents to replace React Cookie Consents.

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -1,48 +1,109 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import getConfig from 'next/config'
+import { useTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
+import { Locale } from 'next-drupal'
+import { useCookies } from 'hds-react'
 
-export const useMatomo = (cookieConsent: string) => {
+export const getConsentStatus = (cookieId: string) => {
+  const { getAllConsents } = useCookies();
+  const consents = getAllConsents();
+  return consents[cookieId];
+};
+
+export const getCookieConsents = (): any => {
+  const { locale } = useRouter()
+  const { t } = useTranslation()
+  const [language, setLanguage] = useState(locale);
+  const onLanguageChange = (newLang: Locale) => setLanguage(newLang);
+
+  return {
+    siteName: t('site_title'),
+    currentLanguage: language,
+    language: {
+      onLanguageChange,
+    },
+    focusTargetSelector: '#focused-element-after-cookie-consent-closed',
+    optionalCookies: {
+      groups: [
+        {
+          commonGroup: 'statistics',
+          cookies: [
+            {
+              commonCookie: 'matomo',
+            },
+            {
+              id: 'matomo_ses',
+              name: '_pk_ses.*',
+              hostName: 'tyollisyyspalvelut.hel.fi',
+              description: 'Matomo-tilastointijärjestelmän eväste.',
+              expiration: '30 minuuttia',
+            },
+            {
+              id: 'rns',
+              name: 'rnsbid',
+              hostName: 'reactandshare.com',
+              description: 'React & Share -reaktionappien toimintaan liittyvä tietue.',
+              expiration: '-',
+            },
+            {
+              id: 'rnsbid_ts',
+              name: 'rnsbid_ts',
+              hostName: 'reactandshare.com',
+              description: 'React & Share -reaktionappien toimintaan liittyvä tietue.',
+              expiration: '-',
+            },
+            {
+              id: 'rns_reaction',
+              name: 'rns_reaction_*',
+              hostName: 'reactandshare.com',
+              description: 'React & Share -reaktionappien toimintaan liittyvä tietue.',
+              expiration: '-',
+            },
+          ],
+        },
+      ],
+    },
+    onAllConsentsGiven: (consents: any) => {
+      // Update Matomo tracking status
+      useMatomo(consents.matomo)
+    },
+  };
+} 
+
+export const useMatomo = (cookieConsent: boolean) => {
   const { MATOMO_URL: url, MATOMO_SITE_ID: siteId } = getConfig().publicRuntimeConfig
-  
-  useEffect(() => {
-    const _paq = (window._paq = window._paq || [])
-    const script = document.createElement("script")
-    script.async = true
-    script.src = `${url}piwik.min.js`
-    script.type = "text/javascript"
 
-    // This means no tracking or cookies unless consent is given.
-    _paq.push(['requireConsent'])
+  const _paq = (window._paq = window._paq || [])
+  const script = document.createElement("script")
+  script.async = true
+  script.src = `${url}piwik.min.js`
+  script.type = "text/javascript"
+  document.head.appendChild(script)
 
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(["setCookieDomain", "*.tyollisyyspalvelut.hel.fi"])
+  // This means no tracking or cookies unless consent is given.
+  _paq.push(['requireConsent'])
 
-    _paq.push(["trackPageView"])
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setCookieDomain", "*.tyollisyyspalvelut.hel.fi"])
 
-    _paq.push(["enableLinkTracking"])
-    _paq.push(["setTrackerUrl", `${url}tracker.php`])
-    _paq.push(["setSiteId", siteId])
+  _paq.push(["trackPageView"])
 
-    // This needs to be set to have any tracking or cookies from matomo.
-    if (cookieConsent === "true") {
-      _paq.push(['setConsentGiven'])
-    }
+  _paq.push(["enableLinkTracking"])
+  _paq.push(["setTrackerUrl", `${url}tracker.php`])
+  _paq.push(["setSiteId", siteId])
 
-    document.head.appendChild(script)
-    return () => {
-      document.head.removeChild(script)
-    }
-
-  }, [cookieConsent])
+  // Enable or disable tracking by the selected consent
+  cookieConsent ? window._paq.push(['setConsentGiven']) : window._paq.push(['forgetConsentGiven'])
 }
 
 export const useReactAndShare = (
-  cookieConsent: string,
+  cookieConsent: boolean | undefined,
   lang: string | undefined,
   pageTitle: string | undefined
 ) => {
   useEffect(() => {
-    if (cookieConsent !== 'true') {
+    if (cookieConsent !== true) {
       return
     }
 
