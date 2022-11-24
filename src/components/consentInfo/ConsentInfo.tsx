e0dc54942f9ce1d3e99e7c35f21e6b19ea4d0ec7
Krister Haverinen <krister.haverinen@siili.com>
2022-11-24 16:05:07 +0200

    THF-406: Fixing app crash when navigating to cookies page.

--- src/components/consentInfo/ConsentInfo.tsx
+++ src/components/consentInfo/ConsentInfo.tsx
@@ -1,6 +1,5 @@
 import { Button, IconAlertCircleFill } from 'hds-react'
 import { useTranslation } from 'next-i18next'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from './consentInfo.module.scss'
 
@@ -12,10 +11,7 @@ export default function ConsentInfo(): JSX.Element {
     <div className={styles.consentInfoWrapper}>
       <h3><IconAlertCircleFill /> {t('consent_info.title')}</h3>
       <p>{t('consent_info.text')}</p>
-
-      <Link href={locale === 'fi' ? '/cookies' : `${locale}/cookies`} passHref>
-        <Button variant="secondary">{t('consent_info.button')}</Button>
-      </Link>
+      <a href={locale === 'fi' ? '/cookies' : `${locale}/cookies`}><Button variant="secondary">{t('consent_info.button')}</Button></a>
     </div>
   )
 }
