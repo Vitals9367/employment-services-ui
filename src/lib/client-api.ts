33296f373988a2f6a37c2d3dba47bca838936dc5
Krister Haverinen <krister.haverinen@siili.com>
2022-12-23 20:29:24 +0200

    THF-413: Adding better fetch for events tags.

--- src/lib/client-api.ts
+++ src/lib/client-api.ts
@@ -7,8 +7,10 @@ import { Locale } from 'next-drupal'
 /** The Client API urls  */
 const EVENTS_URL = '/api/events'
 const EVENTS_SEARCH_URL = '/api/events-search'
+const EVENTS_TAGS_URL = '/api/events-tags'
 const NEWS_URL = '/api/news'
 const UNITS_URL = '/api/units'
+const SEARCH_URL = '/api/search'
 
 export const getEvents = async (queryParams: EventsQueryParams) => {
   const { data } = await axios(`${EVENTS_URL}`, {
@@ -37,3 +39,12 @@ export const getUnits = async (locale: Locale) => {
   return data
 }
 
+export const getSearch = async (index: number, query: string | undefined, locale: Locale) => {
+  const { data } = await axios(`${SEARCH_URL}`, { params: { index: index, q: query, locale: locale } })
+  return data
+}
+
+export const getEventsTags = async () => {
+  const { data } = await axios(`${EVENTS_TAGS_URL}`, {})
+  return data
+}
