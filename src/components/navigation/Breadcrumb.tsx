e717c5a23d4c107399204885473ee907933645e6
Maria Ahokas <maria.ahokas@siili.com>
2023-05-05 08:44:36 +0200

    THF-491: remove breadcrumb.tsx changes

--- src/components/navigation/Breadcrumb.tsx
+++ src/components/navigation/Breadcrumb.tsx
@@ -1,56 +1,39 @@
-import { ReactElement } from 'react';
-import { useTranslation } from 'next-i18next';
-import { IconAngleRight } from 'hds-react';
-import { Link } from 'hds-react';
+import { IconAngleRight, IconArrowRight } from "hds-react"
+import Link from "next/link"
+import { ReactElement } from "react"
+import { useTranslation } from 'next-i18next'
 
-import classNames from '@/lib/classNames';
-import { BreadcrumbContent } from '@/lib/types';
-import { previewNavigation } from '@/lib/helpers';
-import styles from './navigation.module.scss';
+import classNames from "@/lib/classNames"
+import { BreadcrumbContent } from "@/lib/types"
+
+import styles from './navigation.module.scss'
 
 interface BreadcrumbProps {
-  breadcrumb: BreadcrumbContent[];
+  breadcrumb: BreadcrumbContent[]
 }
 
-export const Breadcrumb = ({ breadcrumb }: BreadcrumbProps): JSX.Element => {
-  
-  const { t } = useTranslation('common');
+export const Breadcrumb = ({breadcrumb}:BreadcrumbProps): JSX.Element => {
+  const { t } = useTranslation('common')
 
-  if (!breadcrumb || breadcrumb.length == 0) return <></>;
+  if (!breadcrumb || breadcrumb.length == 0) return <></>
 
   const crumbs: ReactElement[] = breadcrumb.map((crumb, index) => {
-    if (index == breadcrumb.length - 1) {
-      return (
-        <div className={styles.breadcrumbElement} key={crumb.id}>
-          <span>{crumb.title}</span>
-        </div>
-      );
+    if (index == breadcrumb.length-1) {
+      return <div className={styles.breadcrumbElement} key={crumb.id}><span>{crumb.title}</span></div>
     }
     return (
       <div className={styles.breadcrumbElement} key={crumb.id}>
-        <Link
-          href={crumb.url}
-          onClick={() => previewNavigation(crumb.url, true)}
-        >
-          <span>{crumb.title}</span>
-        </Link>
-        <IconAngleRight size="s" aria-hidden="true" />
+        <Link href={crumb.url}><a><span>{crumb.title}</span></a></Link><IconAngleRight size='s' aria-hidden='true'/>
       </div>
-    );
-  });
+    )
+  })
 
   return (
-    <nav
-      className={classNames(styles.breadcrumb)}
-      aria-label={t('navigation.breadcrumb_label')}
-    >
-      <div className={styles.breadcrumbElement} key="breadcrumb-frontpage">
-        <Link href="/" onClick={() => previewNavigation("/", true)}>
-            <span>{t('navigation.frontpage')}</span>
-        </Link>
-        <IconAngleRight size="s" aria-hidden="true" />
+    <nav className={classNames(styles.breadcrumb)} aria-label={t("navigation.breadcrumb_label")}>
+      <div className={styles.breadcrumbElement} key='breadcrumb-frontpage'>
+        <Link href="/"><a><span>{t("navigation.frontpage")}</span></a></Link><IconAngleRight size='s' aria-hidden='true'/>
       </div>
       {crumbs}
     </nav>
-  );
-};
+  )
+}
