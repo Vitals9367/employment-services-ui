939b58c6567f4f8c2586e2be8047214bd2650a88
maria.ahokas <maria.ahokas@siili.com>
2023-07-19 16:25:11 +0300

    THF-559: Update Next, React and Node

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,54 @@
-import { GetStaticPropsContext, GetStaticPropsResult } from 'next'
-import { Locale } from 'next-drupal'
-import getConfig from 'next/config'
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { GetStaticPropsContext, GetStaticPropsResult } from 'next';
+import { Locale } from 'next-drupal';
+import getConfig from 'next/config';
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import { useTranslation } from 'next-i18next';
-import { Container } from 'hds-react'
+import { Container } from 'hds-react';
+import dynamic from 'next/dynamic';
 
-import { getDrupalClient } from '@/lib/drupal-client'
-import getMenu from '@/lib/get-menu'
-import { NODE_TYPES } from '@/lib/drupalApiTypes'
-import { Layout } from '@/components/layout/Layout'
-import NodeLandingPage from '@/components/pageTemplates/NodeLandingPage'
-import { Node, NavProps, FooterProps } from '@/lib/types'
-import { getQueryParamsFor } from '@/lib/params'
+import { getDrupalClient } from '@/lib/drupal-client';
+import { NODE_TYPES } from '@/lib/drupalApiTypes';
+import { Layout } from '@/components/layout/Layout';
+import NodeLandingPage from '@/components/pageTemplates/NodeLandingPage';
+import { Node, NavProps, FooterProps } from '@/lib/types';
+import { getQueryParamsFor } from '@/lib/params';
 import { getDefaultImage, getDescription, getTitle } from '@/lib/helpers';
-import { useConsentStatus, useReactAndShare } from '@/hooks/useAnalytics'
-import ConsentInfo from '@/components/consentInfo/ConsentInfo'
-
+import { useConsentStatus, useReactAndShare } from '@/hooks/useAnalytics';
 
 interface HomePageProps {
-  node: Node
-  nav: NavProps
-  footer: FooterProps
+  node: Node;
+  nav: NavProps;
+  footer: FooterProps;
 }
 
-export async function getStaticProps(context: GetStaticPropsContext): Promise<GetStaticPropsResult<HomePageProps>> {
-  const { locale, defaultLocale } = context as { locale: Locale, defaultLocale: Locale }
-  const { REVALIDATE_TIME, DRUPAL_FRONT_PAGE } = getConfig().serverRuntimeConfig
+export async function getStaticProps(
+  context: GetStaticPropsContext
+): Promise<GetStaticPropsResult<HomePageProps>> {
+  const { locale, defaultLocale } = context as {
+    locale: Locale;
+    defaultLocale: Locale;
+  };
+  const { REVALIDATE_TIME, DRUPAL_FRONT_PAGE } =
+    getConfig().serverRuntimeConfig;
   const drupal = getDrupalClient();
 
   const node = await drupal.getResourceByPath<Node>(DRUPAL_FRONT_PAGE, {
     locale,
     defaultLocale,
-    params: getQueryParamsFor(NODE_TYPES.LANDING_PAGE)
-  })
+    params: getQueryParamsFor(NODE_TYPES.LANDING_PAGE),
+  });
 
   if (!node || (!context.preview && node?.status === false)) {
     return {
       notFound: true,
-      revalidate: 3
-    }
+      revalidate: 3,
+    };
   }
 
-  const langLinks = { fi: '/', en: '/en', sv: '/sv'}
+  const langLinks = { fi: '/', en: '/en', sv: '/sv' };
   const { tree: menu } = await drupal.getMenu('main', {
     locale,
     defaultLocale,
@@ -73,26 +77,33 @@ export async function getStaticProps(context: GetStaticPropsContext): Promise<Ge
       },
       ...(await serverSideTranslations(locale, ['common'])),
     },
-    revalidate: REVALIDATE_TIME
-  }
+    revalidate: REVALIDATE_TIME,
+  };
 }
 
 export default function HomePage({ node, nav, footer }: HomePageProps) {
-  const router = useRouter()
-  const { t } = useTranslation('common')
-  const rnsStatus = useConsentStatus('rns')
-  useReactAndShare(rnsStatus, router.locale, node && getTitle(node, t('site_title')))
+  const ConsentInfo = dynamic(
+    () => import('../src/components/consentInfo/ConsentInfo')
+  );
+  const router = useRouter();
+  const { t } = useTranslation('common');
+  const rnsStatus = useConsentStatus('rns');
+  useReactAndShare(
+    rnsStatus,
+    router.locale,
+    node && getTitle(node, t('site_title'))
+  );
 
   if (!router.isFallback && !node?.id) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
 
-  if (!node) return null
+  if (!node) return null;
 
-  const metaTitle = getTitle(node, t('site_title'))
-  const metaDescription = getDescription(node)
-  const metaUrl = process.env.NEXT_PUBLIC_SITE_URL
-  const metaImage = getDefaultImage(node)
+  const metaTitle = getTitle(node, t('site_title'));
+  const metaDescription = getDescription(node);
+  const metaUrl = process.env.NEXT_PUBLIC_SITE_URL;
+  const metaImage = getDefaultImage(node);
 
   return (
     <Layout header={nav} footer={footer} langcode={node.langcode}>
@@ -109,9 +120,7 @@ export default function HomePage({ node, nav, footer }: HomePageProps) {
       <NodeLandingPage node={node} />
       {/* React and share */}
       <Container className="container hide-print">
-        <div className="rns">
-          {rnsStatus !== true ? <ConsentInfo /> : ''}
-        </div>
+        <div className="rns">{rnsStatus !== true ? <ConsentInfo /> : ''}</div>
       </Container>
     </Layout>
   );
--- src/components/consentInfo/ConsentInfo.tsx
+++ src/components/consentInfo/ConsentInfo.tsx
@@ -1,17 +1,27 @@
-import { Button, IconAlertCircleFill } from 'hds-react'
-import { useTranslation } from 'next-i18next'
-import { useRouter } from 'next/router'
-import styles from './consentInfo.module.scss'
+import { Button, IconAlertCircleFill, Link } from 'hds-react';
+import { useTranslation } from 'next-i18next';
+import router, { useRouter } from 'next/router';
+import styles from './consentInfo.module.scss';
+import { useEffect, useState } from 'react';
 
 export default function ConsentInfo(): JSX.Element {
-  const { t } = useTranslation('common')
-  const { locale } = useRouter()
+  const { t } = useTranslation('common');
+  const { locale } = useRouter();
+  const [cookiesUrl, setCookiesUrl] = useState('/cookies');
+
+  useEffect(() => {
+    setCookiesUrl(locale === 'fi' ? '/cookies' : `/${locale}/cookies)`);
+  }, [locale]);
 
   return (
     <div className={styles.consentInfoWrapper}>
-      <h3><IconAlertCircleFill /> {t('consent_info.title')}</h3>
+      <h3>
+        <IconAlertCircleFill /> {t('consent_info.title')}
+      </h3>
       <p>{t('consent_info.text')}</p>
-      <a href={locale === 'fi' ? '/cookies' : `/${locale}/cookies`}><Button>{t('consent_info.button')}</Button></a>
+      <Button type="button" onClick={() => router.push(cookiesUrl)}>
+        {t('consent_info.button')}
+      </Button>
     </div>
-  )
+  );
 }
