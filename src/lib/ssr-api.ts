0658c58f8e574367f219bc54ffc8dab906ca77e3
maria.ahokas <maria.ahokas@siili.com>
2023-10-11 09:25:34 +0300

    THF-621: Add page limit to 3

--- src/lib/ssr-api.ts
+++ src/lib/ssr-api.ts
@@ -25,7 +25,7 @@ export const getEvents = async (queryParams: EventsQueryParams) => {
     baseEventQueryParams().addSort('field_end_time', 'ASC')
     .addFilter('status', '1')
     .addFilter('langcode', locale)
-    .addPageLimit(4);
+    .addPageLimit(3);
 
   if (tags && locationId) {
     const filteredEventParams = () =>
